09ffd83c991e198826b49196ed440aa3cc917be4
Henrik Hodne <henrik@hodne.io>
2016-01-08 01:13:58 +0000

    gce: Increase .com worker count to 10

--- gce.tf
+++ gce.tf
@@ -2,7 +2,7 @@ provider "google" {}
 
 module "gce_org_prod_workers" {
   source = "github.com/travis-infrastructure/tf_gce_travis_worker"
-  instance_count = 9
+  instance_count = 10
   site = "org"
   environment = "prod"
   machine_type = "g1-small"
