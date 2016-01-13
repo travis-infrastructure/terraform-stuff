2538adbe74058583f6170861d9470e891f4992af
Dan Buch <daniel.buch@gmail.com>
2016-01-13 11:12:58 -0800

    Move some resources back into the production infra

--- production/gce.tf
+++ production/gce.tf
@@ -20,6 +20,26 @@ module "gce_com_prod_workers" {
   image = "${atlas_artifact.travis-worker-gce-image.id}"
 }
 
+module "gce_org_staging_workers" {
+  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
+  instance_count = 1
+  site = "org"
+  environment = "staging"
+  machine_type = "g1-small"
+  zone = "us-central1-b"
+  image = "${atlas_artifact.travis-worker-gce-image.id}"
+}
+
+module "gce_com_staging_workers" {
+  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
+  instance_count = 1
+  site = "com"
+  environment = "staging"
+  machine_type = "g1-small"
+  zone = "us-central1-b"
+  image = "${atlas_artifact.travis-worker-gce-image.id}"
+}
+
 resource "google_compute_instance" "ubuntu_trusty_micro_playground" {
   count = 1
   name = "ubuntu-trusty-micro-playground"
--- staging/atlas.tf
+++ staging/atlas.tf
@@ -1,13 +1,1 @@
 provider "atlas" {}
-
-resource "atlas_artifact" "travis-worker-aws-image" {
-  name = "travis-ci/worker"
-  type = "amazon.image"
-  version = "latest"
-}
-
-resource "atlas_artifact" "travis-worker-gce-image" {
-  name = "travis-ci/worker"
-  type = "google.image"
-  version = "latest"
-}
