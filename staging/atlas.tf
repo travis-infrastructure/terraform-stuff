f7f6b022b10edf3c3baf826358da971f7eb581b9
Henrik Hodne <henrik@travis-ci.com>
2016-03-05 06:49:13 +0100

    staging: move org-staging workers here
    This moves them into a separate GCE project.

--- staging/atlas.tf
+++ staging/atlas.tf
@@ -1,1 +1,7 @@
 provider "atlas" {}
+
+resource "atlas_artifact" "travis-worker-gce-image" {
+  name = "travis-ci/worker"
+  type = "google.image"
+  version = "57"
+}
--- staging/gce.tf
+++ staging/gce.tf
@@ -1,1 +1,17 @@
 provider "google" {}
+
+variable "gce_image_project" {
+  type = "string"
+  description = "The project that contains the GCE worker images"
+}
+
+
+module "gce_org_staging_workers" {
+  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
+  instance_count = 2
+  site = "org"
+  environment = "staging"
+  machine_type = "g1-small"
+  zone = "us-central1-b"
+  image = "${var.gce_image_project}/${atlas_artifact.travis-worker-gce-image.id}"
+}
