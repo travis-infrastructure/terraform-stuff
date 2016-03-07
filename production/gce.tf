14dfbeac8f3e75b10205b85c055505e96de4dea1
Henrik Hodne <henrik@travis-ci.com>
2016-03-07 15:42:19 +0100

    gce: move .com staging to staging GCE project

--- production/gce.tf
+++ production/gce.tf
@@ -30,15 +30,6 @@ module "gce_org_staging_workers" {
   image = "${atlas_artifact.travis-worker-gce-image.id}"
 }
 
-module "gce_com_staging_workers" {
-  source = "github.com/travis-infrastructure/tf_gce_travis_worker"
-  instance_count = 1
-  site = "com"
-  environment = "staging"
-  machine_type = "g1-small"
-  zone = "us-central1-b"
-  image = "${atlas_artifact.travis-worker-gce-image.id}"
-}
 
 resource "google_compute_instance" "ubuntu_trusty_micro_playground" {
   count = 1
