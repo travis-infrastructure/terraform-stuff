163160df8c74e978289cd8086b27645c4d83826a
Henrik Hodne <henrik@travis-ci.com>
2016-03-09 07:05:42 +0100

    gce: decrease worker count for prod-1 projects to 6
    The pool size is increased to 25, so this results in a total pool size
    that's not too different (100 processors less on .org, 60 more on .com).

--- production/gce.tf
+++ production/gce.tf
@@ -2,7 +2,7 @@ provider "google" {}
 
 module "gce_org_prod_workers" {
   source = "github.com/travis-infrastructure/tf_gce_travis_worker"
-  instance_count = 11
+  instance_count = 6
   site = "org"
   environment = "prod"
   machine_type = "g1-small"
@@ -52,7 +52,7 @@ module "gce_org_prod5_workers" {
 
 module "gce_com_prod_workers" {
   source = "github.com/travis-infrastructure/tf_gce_travis_worker"
-  instance_count = 10
+  instance_count = 6
   site = "com"
   environment = "prod"
   machine_type = "g1-small"
